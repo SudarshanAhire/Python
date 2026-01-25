a066df971703f1c11a0f4b7648a997f320c3e573
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-25 23:43:55 +0530

    commit1

---
+++ Magic.py
@@ -0,0 +1,11 @@
+# dunder method / magic method / special method
+
+class Demo:
+    def __init__(self, A):
+        self.No = A
+
+obj1 = Demo(11)
+obj2 = Demo(21)
+
+print(11 + 21)        # 32
+print(obj1 + obj2)    # 
