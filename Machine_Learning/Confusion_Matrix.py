bffe607b14d71c849b147a358dfca37f6ec48864
SudarshanAhire <sudarshanahire292@gmail.com>
2026-02-16 23:21:07 +0530

    commit1

--- Machine_Learning/Confusion_Matrix.py
+++ Machine_Learning/Confusion_Matrix.py
@@ -1,22 +1,18 @@
-from sklearn.datasets import load_iris
-from sklearn.metrics import confusion_metrix
+from sklearn.metrics import confusion_matrix
 
 def main():
     # 1 : Positive
     # 0 : Negative
 
-    Actual = [1, 0, 1, 1, 1, 0, 1, 0, 0, 1]
-    Predicted = [1, 0, 0, 1, 1, 1, 1, 1, 0, 1]
+    Actual =    [1,0,1,1,1,0,1,0,0,1]
+    Predicted = [1,0,0,1,1,1,1,1,0,1]
 
-    print("Actual Data : ", Actual)
-    print("Predicted Data :", Predicted)
+    print("Actual Data : ",Actual)
+    print("Predicted Data : ",Predicted)
 
-    con_mat = confusion_metrix(Actual, Predicted)
+    con_mat = confusion_matrix(Actual,Predicted)
 
     print(con_mat)
-
-
+    
 if __name__ == "__main__":
     main()
-
-
