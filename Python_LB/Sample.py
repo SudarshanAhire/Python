67b9f111f543cd8ef5d6d15c4760f488a5839d88
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-21 14:25:39 +0530

    commit1

---
+++ Python_LB/Sample.py
@@ -0,0 +1,3 @@
+def Display():
+    print("Inside display of Sample.py")
+    print("__name__ from Sample.py is : ", __name__)
---
+++ Python_LB/program981.py
@@ -0,0 +1,16 @@
+# pattern for all application
+
+# change as per thr problem statement 
+
+def Addition(A, B):
+    return A+B
+
+# Fixed
+def main():
+    print("Inside the function")
+    Ret = Addition(10, 11)
+    print("Addition is :", Ret)
+
+# Fixed
+if __name__ == "__main__":
+    main()
